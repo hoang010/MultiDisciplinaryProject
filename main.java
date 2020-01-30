3c9f2fa2a998eaffd12aec76c9f902c8fe98005f
bryanscw <sinbryan1@gmail.com>
2020-01-30 22:56:53 +0800

    Created main class

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main (String[] args){
+
+        // Connect to PC here
+
+        // Connect to Tablet here
+
+        // Connect to Engine(?) here
+
+        // Use camera module
+    }
+}
