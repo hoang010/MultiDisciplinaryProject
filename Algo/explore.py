eab65ce0073d969a4b0417a3ff9f9f43075e917a
bryanscw <sinbryan1@gmail.com>
2020-02-04 17:17:35 +0800

    Conformed update_map to requirements

--- Algo/explore.py
+++ Algo/explore.py
@@ -1,6 +1,10 @@
+import numpy as np
+
+
 class Explore:
     def __init__(self):
-        self.real_map = (0, 0)
+        self.real_map = np.zeros((20, 15))
+        self.explored_map = np.zeros((20, 15))
         self.path = (0, 0)
         self.start = (0, 0)
         self.goal = (0, 0)
@@ -14,13 +18,31 @@ class Explore:
 
     # def move(self):
 
+    def update_map(self, coord_array, obstacle):
+        # TODO: coord_array and obstacle are to be an array of coordinates, i.e. [[x1, y1], [x2, y2]]
+        for x, y in coord_array:
+            self.explored_map[x][y] = 1
+
+        for x, y in obstacle:
+            self.real_map[x][y] = 1
+
     def is_map_complete(self):
-        if self.real_map[0] == (20, 20):
-            return True
+        if self.explored_map.sum() == 300:
+            for i in range(self.explored_map.size()):
+                self.explored_map[i] = ''.join(self.explored_map[i])
+                self.real_map[i] = ''.join(self.real_map[i])
+
+            explored_bin_str = '11' + ''.join(self.explored_map) + '11'
+            real_bin_str = '11' + ''.join(self.real_map) + '11'
+
+            explored_hex_str = hex(int(explored_bin_str, 2))[1:]
+            real_hex_str = hex(int(real_bin_str, 2))[1:]
+            return real_hex_str, explored_hex_str
         return False
 
     def reset(self):
-        self.real_map = (0, 0)
+        self.real_map = np.zeros((20, 15))
+        self.explored_map = np.zeros((20, 15))
         self.path = (0, 0)
         self.start = (0, 0)
         self.goal = (0, 0)
