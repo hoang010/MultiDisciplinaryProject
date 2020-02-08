e81cc657840b935bd9c391e15ee4e24b920b6581
bryanscw <csin001@e.ntu.edu.sg>
2020-02-08 13:02:11 +0800

    Added another algo for exploring (go_to_min) and related functions

--- Algo/explore.py
+++ Algo/explore.py
@@ -4,12 +4,15 @@ import queue
 
 
 class Explore:
-    def __init__(self, direction_class):
+    def __init__(self, map_size, direction_class):
         self.direction_class = direction_class
         self.direction = self.direction_class.N
         self.dir_queue = queue.Queue()
-        self.real_map = np.zeros((20, 15))
-        self.explored_map = np.zeros((20, 15))
+        self.real_map = map_size
+        self.explored_map = self.real_map
+
+        # For go_to_min algo
+        self.min_coord = None
 
         # (x, y, z):
         #   x = left front obstacle
@@ -22,6 +25,60 @@ class Explore:
         # TODO: Not sure about this, how to update?
         self.goal = (0, 0)
         self.current_pos = self.start
+
+    # TODO: For go_to_min algo
+    def update_min_coord(self):
+        min_array = np.where(self.explored_map == np.amin(self.explored_map))
+        min_coord_array = list(zip(min_array[0], min_array[1]))
+        self.min_coord = min_coord_array[0]
+
+    # TODO: For go_to_min algo
+    def coord_diff(self):
+        x_diff = self.current_pos[0][0] - self.min_coord[0]
+        y_diff = self.current_pos[0][1] - self.min_coord[1]
+        return abs(x_diff), abs(y_diff)
+
+    # TODO: For go_to_min algo
+    def go_to_min(self, x_diff, y_diff):
+
+        # Get bool from array index 0, if left front sensor senses obstacle then var takes value 1
+        front_left_obstacle = self.obstacle[0]
+
+        # Get bool from array index 1, if right front sensor senses obstacle then var takes value 1
+        front_right_obstacle = self.obstacle[1]
+
+        # Get bool from array index 2, if right side sensor senses obstacle then var takes value 1
+        right_obstacle = self.obstacle[2]
+
+        # Initialise obstacle coordinate array to store obstacle coordinate if any
+        obstacle_coord = None
+
+        if right_obstacle:
+            if y_diff != 0:
+                if not front_right_obstacle and front_left_obstacle:
+                    self.dir_queue.put('advance')
+                    self.update_pos()
+                else:
+                    # Get obstacle coordinate
+                    # If left_obstacle, get obstacle coordinate wrt front left robot coordinate
+                    # Else get obstacle coordinate wrt front right robot coordinate
+                    obstacle_coord = self.get_obstacle_coord(0 if front_left_obstacle else 1)
+                    self.dir_queue.put('left')
+                    self.left_update_dir()
+        else:
+            if x_diff != 0:
+                if not front_left_obstacle and not front_right_obstacle:
+                    self.dir_queue.put('advance')
+                    self.update_pos()
+                else:
+                    # Get obstacle coordinate
+                    # If left_obstacle, get obstacle coordinate wrt front left robot coordinate
+                    # Else get obstacle coordinate wrt front right robot coordinate
+                    obstacle_coord = self.get_obstacle_coord(0 if front_left_obstacle else 1)
+                    self.dir_queue.put('right')
+                    self.right_update_dir()
+
+        self.update_map(self.current_pos, obstacle_coord)
 
     def right_wall_hugging(self):
 
@@ -37,7 +94,7 @@ class Explore:
             self.dir_queue.put('right')
 
             # Update robot direction
-            self.update_dir()
+            self.right_update_dir()
 
         # If there is an obstacle in front
         elif front_left_obstacle or front_right_obstacle:
@@ -51,7 +108,7 @@ class Explore:
             self.dir_queue.put('left')
 
             # Update robot direction
-            self.update_dir()
+            self.left_update_dir()
 
         # If no obstacle
         else:
@@ -98,7 +155,7 @@ class Explore:
             for x, y in obstacle:
                 self.real_map[x][y] = 1
 
-    def update_dir(self):
+    def left_update_dir(self):
         # If current direction is North, North turning to left is West
         if self.direction == self.direction_class.N:
             # Change current direction to West
@@ -108,6 +165,27 @@ class Explore:
         elif self.direction == self.direction_class.S:
             # Change current direction to East
             self.direction = self.direction_class.E
+
+        # If current direction is East, North turning to left is South
+        elif self.direction == self.direction_class.E:
+            # Change current direction to South
+            self.direction = self.direction_class.S
+
+        # If current direction is West, North turning to left is North
+        else:
+            # Change current direction to North
+            self.direction = self.direction_class.N
+
+    def right_update_dir(self):
+        # If current direction is North, North turning to left is East
+        if self.direction == self.direction_class.N:
+            # Change current direction to East
+            self.direction = self.direction_class.E
+
+        # If current direction is South, North turning to left is West
+        elif self.direction == self.direction_class.S:
+            # Change current direction to West
+            self.direction = self.direction_class.W
 
         # If current direction is East, North turning to left is North
         elif self.direction == self.direction_class.E:
