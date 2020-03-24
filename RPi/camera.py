6daaad57b8dad2794296148217e842019d63f9da
Kian How <n.kianhow@gmail.com>
2020-03-24 13:13:49 +0800

    Update camera configuration

--- RPi/camera.py
+++ RPi/camera.py
@@ -1,13 +1,15 @@
 from picamera import PiCamera
 from picamera.array import PiRGBArray
+import cv2 as cv
 
 class Camera:
 
 	def __init__(self , resolution=(640, 480), framerate = 32):
-		self.filepath = "/home/pi/src/main/G17/images/"
+		self.filepath = "/home/pi/src/main/G17/Algo/images/"
 		self.camera = PiCamera()
 		self.camera.resolution = resolution
 		self.camera.framerate = framerate
+		self.rawCapture = PiRGBArray(self.camera)
 
 		#self.camera.vflip = False
 		#self.camera.hflip = False
@@ -19,9 +21,13 @@ class Camera:
 		A function to capture the image and stored it at an assigned directory
 		with its respective number count.
 		"""
-		filename = F"{filepath}img{self.counter}.jpg"
-		self.camera.capture(filename)
+		filename = "{}img{}.jpg".format(self.filepath, str(self.counter))
+		# filename = F"{filepath}img{self.counter}.jpg".
+		self.camera.capture(self.rawCapture, format="bgr")
+		image = self.rawCapture.array
+		cv.imwrite(filename, image)
 		self.counter = self.counter + 1
+		self.rawCapture.truncate(0)
 
 	def close(self):
 		"""
